e1e3c12139157aadbe8ae86ee66baf3b4a25f0b5
Akaps316 <48785708+Akaps316@users.noreply.github.com>
2024-03-21 14:51:08 +0530

    Added requests specific to latest movie, popular movies and movie details

--- Sources/NetworkSDK/NetworkSDK.swift
+++ Sources/NetworkSDK/NetworkSDK.swift
@@ -1,46 +1,105 @@
+// NetworkSDK.swift
+
 import Foundation
 
 public class NetworkSDK {
+    static let shared = NetworkSDK()
     
-    public static let shared = NetworkSDK()
+    private var apiKey = "909594533c98883408adef5d56143539"
+    private var baseURL = "https://api.themoviedb.org/3"
     
-    private let baseURL: String
-    private let apiKey: String
+    private init() {}
     
-    public init(baseURL: String = "https://api.themoviedb.org/3", apiKey: String = "909594533c98883408adef5d56143539") {
-        self.baseURL = baseURL
-        self.apiKey = apiKey
+    func setApiKey(newApiKey: String)
+    {
+        apiKey = newApiKey
     }
     
-    public func fetchMoviesList(completion: @escaping ([Movie]?, Error?) -> Void) {
-        let urlString = "\(baseURL)/movie/popular?api_key=\(apiKey)"
-        guard let url = URL(string: urlString) else {
-            completion(nil, NSError(domain: "Invalid URL", code: 0, userInfo: nil))
+    func setBaseURL(newBaseURL: String)
+    {
+        baseURL = newBaseURL
+    }
+    
+    enum Endpoint {
+        case latestMovies
+        case popularMovies
+        case movieDetail(movieId: Int)
+        
+        var path: String {
+            switch self {
+            case .latestMovies:
+                return "/movie/latest"
+            case .popularMovies:
+                return "/movie/popular"
+            case .movieDetail(let movieId):
+                return "/movie/\(movieId)"
+            }
+        }
+        
+        var queryItems: [URLQueryItem] {
+            return [
+                URLQueryItem(name: "api_key", value: NetworkSDK.shared.apiKey)
+            ]
+        }
+    }
+    
+    func fetchLatestMovies(completion: @escaping (Movie?, Error?) -> Void) {
+        fetchData(for: .latestMovies, completion: completion)
+    }
+    
+    func fetchPopularMovies(completion: @escaping (MovieResponse?, Error?) -> Void) {
+        fetchData(for: .popularMovies, completion: completion)
+    }
+    
+    func fetchMovieDetail(for movieId: Int, completion: @escaping (Movie?, Error?) -> Void) {
+        fetchData(for: .movieDetail(movieId: movieId)) { (movie: Movie?, error: Error?) in
+            completion(movie, error)
+        }
+    }
+    
+    private func fetchData<T: Decodable>(for endpoint: Endpoint, completion: @escaping (T?, Error?) -> Void) {
+        guard var urlComponents = URLComponents(string: baseURL + endpoint.path) else {
+            completion(nil, NSError(domain: "NetworkSDK", code: -1, userInfo: [NSLocalizedDescriptionKey: "Invalid URL"]))
+            return
+        }
+        
+        urlComponents.queryItems = endpoint.queryItems
+        
+        guard let url = urlComponents.url else {
+            completion(nil, NSError(domain: "NetworkSDK", code: -1, userInfo: [NSLocalizedDescriptionKey: "Invalid URL"]))
             return
         }
         
         URLSession.shared.dataTask(with: url) { data, response, error in
-            guard let data = data, error == nil else {
+            guard let data = data else {
                 completion(nil, error)
                 return
             }
             
             do {
-                let decoder = JSONDecoder()
-                let movies = try decoder.decode(MoviesResponse.self, from: data)
-                completion(movies.results, nil)
+                let decodedData = try JSONDecoder().decode(T.self, from: data)
+                completion(decodedData, nil)
             } catch {
+                print(error)
                 completion(nil, error)
             }
         }.resume()
     }
 }
 
-public struct Movie: Codable {
+public struct Movie: Decodable {
+    public let id: Int
     public let title: String
     public let overview: String
+    public let release_date: String
+    public let poster_path: String?
+    // Add other properties as needed
+    
 }
 
-public struct MoviesResponse: Codable {
-    let results: [Movie]
+public struct MovieResponse: Decodable {
+    public let page: Int
+    public let results: [Movie]
+    public let total_pages: Int
+    public let total_results: Int
 }
--- Tests/NetworkSDKTests/NetworkSDKTests.swift
+++ Tests/NetworkSDKTests/NetworkSDKTests.swift
@@ -1,180 +1,114 @@
 import XCTest
 @testable import NetworkSDK
 
-final class NetworkSDKTests: XCTestCase {
-    func testExample() throws {
-        // XCTest Documentation
-        // https://developer.apple.com/documentation/xctest
-
-        // Defining Test Cases and Test Methods
-        // https://developer.apple.com/documentation/xctest/defining_test_cases_and_test_methods
-    }
-
-
-    func testFetchMoviesListSuccess() {
-        // Given
-        let expectation = self.expectation(description: "Fetch movies list")
-        var fetchedMovies: [Movie]?
+class NetworkSDKTests: XCTestCase {
+    
+    func testFetchLatestMovies_Success() {
+        let expectation = self.expectation(description: "Fetch latest movies")
+        var fetchedMovies: Movie?
         var fetchError: Error?
-
-        // When
-        NetworkSDK.shared.fetchMoviesList { (movies, error) in
+        
+        NetworkSDK.shared.fetchLatestMovies { (movies, error) in
             fetchedMovies = movies
             fetchError = error
             expectation.fulfill()
         }
-
-        // Then
+        
         waitForExpectations(timeout: 5, handler: nil)
-        XCTAssertNotNil(fetchedMovies, "Fetched movies should not be nil")
-        XCTAssertNil(fetchError, "Error fetching movies: \(fetchError?.localizedDescription ?? "")")
+        
+        XCTAssertNotNil(fetchedMovies)
+        XCTAssertNil(fetchError)
     }
-
-    func testFetchMoviesListFailureInvalidAPIKey() {
-        // Given
-        let expectation = self.expectation(description: "Fetch movies list")
-        var fetchedMovies: [Movie]?
+    
+    func testFetchPopularMovies_Success() {
+        let expectation = self.expectation(description: "Fetch latest movies")
+        var fetchedMovies: MovieResponse?
         var fetchError: Error?
-
-        // When
-        let invalidApiKey = "INVALID_API_KEY"
-        let networkSDK = NetworkSDK(apiKey: invalidApiKey)
-        networkSDK.fetchMoviesList { (movies, error) in
+        
+        NetworkSDK.shared.fetchPopularMovies { (movies, error) in
             fetchedMovies = movies
             fetchError = error
             expectation.fulfill()
         }
-        // Then
+        
         waitForExpectations(timeout: 5, handler: nil)
-        XCTAssertNil(fetchedMovies, "Fetched movies should be nil")
-        XCTAssertNotNil(fetchError, "Expected error fetching movies")
+        
+        XCTAssertNotNil(fetchedMovies)
+        XCTAssertNil(fetchError)
     }
-
-    func testFetchMoviesListFailureInvalidURL() {
-        // Given
-        let expectation = self.expectation(description: "Fetch movies list")
-        var fetchedMovies: [Movie]?
+    
+    func testFetchLatestMovies_InvalidAPIKey() {
+        let expectation = self.expectation(description: "Fetch latest movies with invalid API key")
         var fetchError: Error?
-
-        // When
-        let invalidURL = "invalid-url"
-        let networkSDK = NetworkSDK(baseURL: invalidURL)
-        networkSDK.fetchMoviesList { (movies, error) in
-            fetchedMovies = movies
+        
+        // Replace API key with an invalid key
+        NetworkSDK.shared.setApiKey(newApiKey: "invalid_api_key")
+        
+        NetworkSDK.shared.fetchLatestMovies { (movies, error) in
             fetchError = error
             expectation.fulfill()
         }
-
-        // Then
+        
         waitForExpectations(timeout: 5, handler: nil)
-        XCTAssertNil(fetchedMovies, "Fetched movies should be nil")
-        XCTAssertNotNil(fetchError, "Expected error fetching movies")
+        
+        XCTAssertNotNil(fetchError)
+        XCTAssertEqual((fetchError! as NSError).code, 4865)
     }
-
-    func testFetchMoviesListFailureNoInternetConnection() {
-        // Given
-        let expectation = self.expectation(description: "Fetch movies list")
-        var fetchedMovies: [Movie]?
+    
+    func testFetchLatestMovies_InvalidURL() {
+        let expectation = self.expectation(description: "Fetch latest movies with invalid URL")
         var fetchError: Error?
-
-        // When
-        // Simulate no internet connection by using invalid host
-        let invalidHost = "http://invalidhost"
-        let networkSDK = NetworkSDK(baseURL: invalidHost)
-        networkSDK.fetchMoviesList { (movies, error) in
-            fetchedMovies = movies
+        
+        // Replace baseURL with an invalid URL
+        NetworkSDK.shared.setBaseURL(newBaseURL: "invalid_url")
+        
+        NetworkSDK.shared.fetchLatestMovies { (movies, error) in
             fetchError = error
             expectation.fulfill()
         }
-
-        // Then
+        
         waitForExpectations(timeout: 5, handler: nil)
-        XCTAssertNil(fetchedMovies, "Fetched movies should be nil")
-        XCTAssertNotNil(fetchError, "Expected error fetching movies")
+        
+        XCTAssertNotNil(fetchError)
+        XCTAssertEqual((fetchError! as NSError).code, -1002)
     }
-
-    func testFetchMoviesListFailureEmptyResponse() {
-        // Given
-        let expectation = self.expectation(description: "Fetch movies list")
-        var fetchedMovies: [Movie]?
+    
+    func testFetchLatestMovies_NoInternetConnection() {
+        // Simulate no internet connection
+        // (This test assumes that the device is not connected to the internet)
+        let networkSDK = NetworkSDK.shared
+        
+        let expectation = self.expectation(description: "Fetch latest movies with no internet connection")
         var fetchError: Error?
-
-        // When
-        // Simulate empty response
-        let networkSDK = NetworkSDK(baseURL: "https://www.emptyresponse.com")
-        networkSDK.fetchMoviesList { (movies, error) in
-            fetchedMovies = movies
+        
+        networkSDK.fetchLatestMovies { (movies, error) in
             fetchError = error
             expectation.fulfill()
         }
-
-        // Then
+        
         waitForExpectations(timeout: 5, handler: nil)
-        XCTAssertNil(fetchedMovies, "Fetched movies should be nil")
-        XCTAssertNotNil(fetchError, "Expected error fetching movies")
+        
+        XCTAssertNotNil(fetchError)
+        XCTAssertEqual((fetchError! as NSError).code, -1002) // NSURLErrorNotConnectedToInternet
     }
-
-    func testFetchMoviesListFailureServerError() {
-        // Given
-        let expectation = self.expectation(description: "Fetch movies list")
-        var fetchedMovies: [Movie]?
+    
+    func testFetchMovieDetail_Success() {
+        let expectation = self.expectation(description: "Fetch movie detail")
+        var fetchedMovie: Movie?
         var fetchError: Error?
-
-        // When
-        // Simulate server error
-        let networkSDK = NetworkSDK(baseURL: "https://www.servererror.com")
-        networkSDK.fetchMoviesList { (movies, error) in
-            fetchedMovies = movies
+        
+        let movieId = 1262535
+        
+        NetworkSDK.shared.fetchMovieDetail(for: movieId) { (movie, error) in
+            fetchedMovie = movie
             fetchError = error
             expectation.fulfill()
         }
-
-        // Then
+        
         waitForExpectations(timeout: 5, handler: nil)
-        XCTAssertNil(fetchedMovies, "Fetched movies should be nil")
-        XCTAssertNotNil(fetchError, "Expected error fetching movies")
+        
+        XCTAssertNotNil(fetchedMovie)
+        XCTAssertNil(fetchError)
     }
 
-    func testFetchMoviesListFailureTimeout() {
-        // Given
-        let expectation = self.expectation(description: "Fetch movies list")
-        var fetchedMovies: [Movie]?
-        var fetchError: Error?
-
-        // When
-        // Simulate timeout
-        let networkSDK = NetworkSDK(baseURL: "https://www.delayedresponse.com")
-        networkSDK.fetchMoviesList { (movies, error) in
-            fetchedMovies = movies
-            fetchError = error
-            expectation.fulfill()
-        }
-
-        // Then
-        waitForExpectations(timeout: 5, handler: nil)
-        XCTAssertNil(fetchedMovies, "Fetched movies should be nil")
-        XCTAssertNotNil(fetchError, "Expected error fetching movies")
-    }
-
-    func testFetchMoviesListDecoding() {
-        // Given
-        let expectation = self.expectation(description: "Fetch movies list")
-        var fetchedMovies: [Movie]?
-        var fetchError: Error?
-
-        // When
-        // Simulate successful response with invalid JSON
-        let networkSDK = NetworkSDK(baseURL: "https://www.invalidjsonresponse.com")
-        networkSDK.fetchMoviesList { (movies, error) in
-            fetchedMovies = movies
-            fetchError = error
-            expectation.fulfill()
-        }
-
-        // Then
-        waitForExpectations(timeout: 5, handler: nil)
-        XCTAssertNil(fetchedMovies, "Fetched movies should be nil")
-        XCTAssertNotNil(fetchError, "Expected error fetching movies")
-    }
 }
-
